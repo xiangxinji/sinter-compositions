c8fb37d18a62ab63114c92cb827d0bb05cbc3941
向歆纪 <xxjbbking@163.com>
2021-09-26 16:17:46 +0800

    修复问题

--- src/base/helper.ts
+++ src/base/helper.ts
@@ -20,3 +20,29 @@ export function camelToKebabCase(str: string) {
     if (r[0] === "-") return r.substr(1);
     return r;
 }
+
+
+export function exportFile(res: Blob, name: string) {//二进制文件流导出
+    const url = window.URL.createObjectURL(new Blob([res], {type: 'application/vnd.ms-excel;charset=utf-8'}))
+    const link = document.createElement('a')
+    link.style.display = 'none'
+    link.href = url
+    let nd = new Date();
+    const fileName = name || (nd.getFullYear() + "-" + (nd.getMonth() + 1) + "-" + nd.getDate())
+    link.setAttribute('download', fileName + '.xlsx')
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    window.URL.revokeObjectURL(url);
+}
+
+
+export function exportPath(path: string) {//通过文件地址下载
+    const elink = document.createElement("a");
+    elink.style.display = "none";
+    elink.href = path
+    document.body.appendChild(elink);
+    elink.click();
+    URL.revokeObjectURL(elink.href); // 释放URL 对象
+    document.body.removeChild(elink);
+}
